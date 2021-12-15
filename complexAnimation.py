bbe10f544fbd4496f628c3da97568bab647c0e6b
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-15 10:19:11 -0700

    Update complexAnimation.py

--- complexAnimation.py
+++ complexAnimation.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class animation:
     """Create an animation that holds keyframes"""
-    def __init__(self, width=1920, height=1080):
-        self.width, self.height = width, height
-        self.keyframes = []
+    def __init__(self, width=1920, height=1080, colorfunc=None):
+        self.width, self.height = width, height #resolution of animation in pixels
+        self.keyframes = [] #list of all keyframes
 
 class keyframe:
     """create a keyframe that holds data about the function, length, etc."""
-    def __init__(self, )
+    def __init__(self, blCorner=(-1,-1), trCorner=(1,1),function = lambda x:x, length = 60):
+        pass
