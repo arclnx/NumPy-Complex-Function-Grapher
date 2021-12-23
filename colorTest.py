a763f1a4100f94bcb319820d4c6bf3a8b2db2f24
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-23 11:36:46 -0700

    Update colorTest.py

--- colorTest.py
+++ colorTest.py
@@ -1,5 +1,6 @@
 from PIL import Image as im
 import numpy as np
+import colorsys as color
 
 def ComplexToRGB1(complex):
     angle = np.angle(complex,deg=True)
@@ -7,4 +8,12 @@ def ComplexToRGB1(complex):
     return angle
 
 def ComplexToRGB2(complex):
-    return (np.angle(complex,deg=True)+360)%360
+    angle = ((np.angle(complex,deg=True)+360)%360)/360
+    magnitude = np.absolute(complex)
+    return color.hls_to_rgb(angle,0.5,1-np.log2(magnitude)%1)
+
+test=np.linspace(-1-1j,1+1j,11)
+ComplexToRGB2Vector = np.vectorize(ComplexToRGB2)
+print("start")
+print(ComplexToRGB2Vector(test))
+print("done")
