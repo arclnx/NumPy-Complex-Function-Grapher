3e85aa62a128190eb2e6f595b2981ddf2064eb0a
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-23 10:17:56 -0700

    Update colorTest.py

--- colorTest.py
+++ colorTest.py
@@ -1,6 +1,10 @@
 from PIL import Image as im
 import numpy as np
 
-def ComplexToRGB(complex):
-    phase = np.angle(complex,deg=True)
-    
+def ComplexToRGB1(complex):
+    angle = np.angle(complex,deg=True)
+    angle = angle if angle<=0 else angle+180
+    return angle
+
+def ComplexToRGB2(complex):
+    return (np.angle(complex,deg=True)+360)%360
