3cc976d5ed578d45095eabb576c1623e4ff84c87
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-25 12:34:22 -0700

    Update colorTest.py

--- colorTest.py
+++ colorTest.py
@@ -1,19 +1,33 @@
-from PIL import Image as im
 import numpy as np
-import colorsys as color
+import colorsys as colorsys
 
-def ComplexToRGB1(complex):
-    angle = np.angle(complex,deg=True)
-    angle = angle if angle<=0 else angle+180
-    return angle
+def hsl_to_rgb2(H,S,L):
+    def max2(a,b):
+        return np.where(a>b,a,b)
+    
+    def min2(a,b):
+        return np.where(a<b,a,b)
+
+    def f(n):
+        k = (n + H/30)%12
+        a = S * min2(L,1-L)
+        return L - a * max2(-1,min2(k-3,min2(9-k,1)))
+    return (f(0),f(8),f(4))
+
+def ComplexToRGB(complex):
+    angle = ((np.angle(complex,deg=True)+360)%360)/360
+    magnitude = np.absolute(complex)
+    rawColor =  colorsys.hls_to_rgb(angle,0.5,1-np.log2(magnitude)%1)
+    return rawColor
 
 def ComplexToRGB2(complex):
     angle = ((np.angle(complex,deg=True)+360)%360)/360
     magnitude = np.absolute(complex)
-    return color.hls_to_rgb(angle,0.5,1-np.log2(magnitude)%1)
+    rawColor =  hsl_to_rgb2(angle,1-np.log2(magnitude)%1,.5)
+    return rawColor
 
-test=np.linspace(-1-1j,1+1j,11)
-ComplexToRGB2Vector = np.vectorize(ComplexToRGB2)
-print("start")
-print(ComplexToRGB2Vector(test))
-print("done")
+foo = np.linspace(-1-1j,1+1j,3)
+
+#print(ComplexToRGB2(foo))
+print(colorsys.hls_to_rgb(.5,.6,1))
+print(hsl_to_rgb2(180,1,.6))
