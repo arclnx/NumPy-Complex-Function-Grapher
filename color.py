b426d2ccba79660ed7709108593a7cfca9cb4826
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-27 13:46:46 -0700

    Update color.py

--- color.py
+++ color.py
@@ -1,10 +1,11 @@
 import numpy as np
-import timeit
 
 def hsl_to_rgb2(H,S,L):
+    """takes H,S,L inputs, where H is [0,360], and S,L are [0,1]"""
+
     def max2(a,b):
         return np.where(a>b,a,b)
-    
+
     def min2(a,b):
         return np.where(a<b,a,b)
 
@@ -12,24 +13,15 @@ def hsl_to_rgb2(H,S,L):
         k = (n + H/30)%12
         a = S * min2(L,1-L)
         return L - a * max2(-1,min2(k-3,min2(9-k,1)))
+
     return (f(0),f(8),f(4))
 
 def ComplexToRGB(complex):
+    """takes a complex number and returns a tuple of RGB values in the range [0,1]"""
     angle = ((np.angle(complex,deg=True)+360)%360)/360
     magnitude = np.absolute(complex)
     rawColor =  hsl_to_rgb2(angle,1-np.log2(magnitude)%1,.5)
     return rawColor
 
-real = np.linspace(-1,1,1920)
-complex = np.linspace(-1,1,1080)
-real, complex = np.meshgrid(real, complex, indexing='ij')
-cplane = real + complex*1j
-
-print(cplane)
-#print(ComplexToRGB2(foo))
-print("start")
-
-for i in range(10):
-    ComplexToRGB(cplane)
-
-print("done")
+print(np.absolute(np.inf+2j))
+print(np.log2(np.inf))
