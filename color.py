3fa4609ca471ae0560460d2b9494861ed1eda33a
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-12-28 19:06:29 -0700

    Finished rendering
    It works!
    Finally got rendering to work, but it's just not rendering all the frames

--- color.py
+++ color.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def hsl_to_rgb2(H,S,L):
-    """takes H,S,L inputs, where H is [0,360], and S,L are [0,1]"""
+    """takes H,S,L inputs, where H is [0,360], and S,L are [0,1], and returns integer RGB values in the form RGB, R,G,B are [0,255]"""
 
     def max2(a,b):
         return np.where(a>b,a,b)
@@ -14,14 +14,21 @@ def hsl_to_rgb2(H,S,L):
         a = S * min2(L,1-L)
         return L - a * max2(-1,min2(k-3,min2(9-k,1)))
 
-    return (f(0),f(8),f(4))
+    return np.array([f(0)*255,f(8)*255,f(4)*255]).astype(np.uint8)
 
 def ComplexToRGB(complex):
-    """takes a complex number and returns a tuple of RGB values in the range [0,1]"""
-    angle = ((np.angle(complex,deg=True)+360)%360)/360
+    """takes a complex number and returns a tuple of integer RGB values in the range [0,255]"""
+    angle = np.nan_to_num(((np.angle(complex,deg=True)+360)%360))
     magnitude = np.absolute(complex)
-    rawColor =  hsl_to_rgb2(angle,1-np.log2(magnitude)%1,.5)
+    rawColor =  hsl_to_rgb2(angle,1-np.nan_to_num(np.log2(magnitude))%1,.5)
     return rawColor
 
-print(np.absolute(np.inf+2j))
-print(np.log2(np.inf))
+real = np.linspace(-1,1,10)
+complex = np.linspace(-1,1,10)
+real, complex = np.meshgrid(real, complex, indexing='ij')
+cplane = real + complex*1j
+print(ComplexToRGB(cplane))
+print("--------")
+print(ComplexToRGB(cplane).T)
+print(ComplexToRGB(cplane).T.shape)
+print(ComplexToRGB(cplane).T[0][0])
--- complexAnimation.py
+++ complexAnimation.py
@@ -1,6 +1,5 @@
-import colorsys
 import numpy as np
-from PIL import Image as im
+from PIL import Image
 import color
 
 class animation:
@@ -12,25 +11,49 @@ class animation:
         self.keyframes.append(keyframe)
         pass
 
-    def render(self, size=(1920,1080)):
+    def render(self, size=(1920,1080), folderPath=""):
         # create an array of the data fro each keyframe
         keyframeData = np.array([keyframe.calculate(size) for keyframe in self.keyframes])
+        
         # create an array of the lengths of each keyframe
         keyframeLengths = np.array([keyframe.length for keyframe in self.keyframes])
+        
         # set up frame data array as an empty 3d array with the correct width and height, and a depth of 0
         frameData = np.empty((0,size[0],size[1]))
-        # loop through each keyframe, interpolate the interframes, and append them to frameData
-        for frameIndex in range(len(self.keyframes)-1):
+        
+        # loop through each keyframe, interpolate the interframes, and render them
+        frameNumber = 0
+        for keyFrameIndex in range(len(self.keyframes)-1):
+            
+            #set frameData to an array of 2d arrays of each frame
+            frameData =  np.linspace(start=keyframeData[keyFrameIndex],
+                                     stop=keyframeData[keyFrameIndex+1],
+                                     num=keyframeLengths[keyFrameIndex]-1,
+                                     endpoint=False)
+            for frame in frameData:
+                print(frame)
+                print("--------------------------------------")
+                # apply the complex->RGB function, then transpose to get the right shape
+                renderedFrame = color.ComplexToRGB(frame)
+                print(renderedFrame)
+                print(renderedFrame.shape)
+                print("--------------------------------------")
+                print(renderedFrame.T)
+                print(renderedFrame.T.ndim)
+                # convert the np.ndarray to a PIL image
+                frameImage = Image.fromarray(renderedFrame.T,"RGB")
+                
+                # set the filepath
+                filePath = "C:\\Users\\trevo\\Desktop\\Grapher_Output\\" + str(frameNumber).rjust(4,"0") + ".png"
+                # save the image
+                frameImage.save(filePath)
+                print("saved at: " + filePath)
 
-            frameData = np.concatenate((frameData,
-                                        np.linspace(start=keyframeData[frameIndex],
-                                                    stop=keyframeData[frameIndex+1],
-                                                    num=keyframeLengths[frameIndex]-1,
-                                                    endpoint=False)))
-        frameData = np.concatenate((frameData, np.array([keyframeData[-1]])))
-        output = open(r"C:\Users\trevo\Desktop\output.txt", "w")
-        output.write(str(frameData))
-        output.close()
+        #frameData = np.concatenate((frameData, np.array([keyframeData[-1]])))
+        #output = open(r"C:\Users\trevo\Desktop\output.txt", "w")
+        #output.write(str(frameData))
+        #output.close()
+        #print(frameData.shape)
 
 class keyframe:
     """create a keyframe that holds data about the function, length, etc."""
@@ -73,5 +96,4 @@ a = keyframe(length=4)
 b = keyframe(length=4)
 myanim.addKeyframe(a)
 myanim.addKeyframe(b)
-print(a.length)
-myanim.render((2,4))
+myanim.render((5,5))
