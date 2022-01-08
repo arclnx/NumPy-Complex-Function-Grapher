2d3562fea9d041463c1cc240f0f00586c5899caa
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-01-08 11:39:52 -0700

    Update myAnimation.py

--- myAnimation.py
+++ myAnimation.py
@@ -1,8 +1,7 @@
 import complexAnimation as anim
 myanim = anim.animation()
-a = anim.keyframe(function ="x", length=4)
-b = anim.keyframe(function = "1j/x", length=4)
+a = anim.keyframe(function ="x", length=60)
+b = anim.keyframe(function = "np.sin(x**2)", length=4)
 myanim.addKeyframe(a)
 myanim.addKeyframe(b)
 myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
-#sdf
