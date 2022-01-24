293d8b490ccaf55ebd5d66dd262ae792434ec723
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-01-24 09:48:33 -0700

    Update myAnimation.py

--- myAnimation.py
+++ myAnimation.py
@@ -2,9 +2,9 @@ import complexAnimation as anim
 
 myanim = anim.animation()
 a = anim.keyframe(function = "x",          smin=(-8,-8), max=(8,8), length=60)
-b = anim.keyframe(function = "np.sinh(x)", min=(-8,-8), max=(8,8), length=4)
+b = anim.keyframe(function = "(x**2-1)/(x**2+1)", min=(-8,-8), max=(8,8), length=4)
 myanim.addKeyframe(a)
 myanim.addKeyframe(b)
 
-myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=x to f(x)=sinh(x)")
+myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=x to f(x)=(x^2-1) div (x^2+1)")
 #myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
