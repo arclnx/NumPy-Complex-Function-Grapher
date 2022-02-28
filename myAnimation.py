5f04aae39249cef69b944cad5d2b010bd04bcaee
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-02-28 11:25:36 -0700

    Rendered new animatation

--- myAnimation.py
+++ myAnimation.py
@@ -2,9 +2,9 @@ import complexAnimation as anim
 
 myanim = anim.animation()
 a = anim.keyframe(function = "x",                 min=(-8,-8), max=(8,8), length=60)
-b = anim.keyframe(function = "e**(np.pi*x)", min=(-8,-8), max=(8,8), length=4)
+b = anim.keyframe(function = "np.e**(np.pi*x)", min=(-8,-8), max=(8,8), length=4)
 myanim.addKeyframe(a)
 myanim.addKeyframe(b)
 
-myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=x to f(x)=e^(pi*x)")
+myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=x to f(x)=e^(pi x)")
 #myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
