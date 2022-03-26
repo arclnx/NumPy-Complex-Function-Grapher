98451f84739a3fbb7fdb29ab5d7d1bec9699979a
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-03-26 12:25:23 -0700

    Update myAnimation.py

--- myAnimation.py
+++ myAnimation.py
@@ -1,12 +1,13 @@
 import complexAnimation as anim
 
+
+
+
 myanim = anim.animation()
 a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
-b = anim.keyframe(function = "2**x",                              min=(-8,-8), max=(8,8), length=4)
 myanim.addKeyframe(a)
-#myanim.addKeyframe(b)
 
-# "(1-x)**(2/(x**2+1j))/((2-x)**2)"
+
 
 myanim.render(size=(1440*5,1440*5), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
 #myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
