71a961fb736e1e84642d7d2c4ea8e447485ea1f7
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-03-27 15:57:10 -0700

    Update mosaic.py

--- mosaic.py
+++ mosaic.py
@@ -1,4 +1,6 @@
 import complexAnimation as anima
 
-def mosaic(function, totalSize, min, max, mosaicTiles, folderPath):
-    
+def mosaic(function, totalSize, min, max, tiledSize, folderPath):
+    tiledWidth = tiledSize[0]
+    tiledHeight = tiledSize[1]
+    mins=[]
