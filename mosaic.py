f4221c05cdba42fa6cd96031f5028922641869c9
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-04-26 10:08:07 -0700

    Added mosaic

--- mosaic.py
+++ mosaic.py
@@ -4,7 +4,7 @@ import numpy as np
 def mosaic(function, pixelSize, min, max, tiledSize, folderPath):
     tiledWidth = tiledSize[0]
     tiledHeight = tiledSize[1]
-    tilePixelSize = (pixelSize(0)/tiledWidth,pixelSize(1)/tiledHeight)
+    tilePixelSize = (pixelSize[0]/tiledWidth,pixelSize[0]/tiledHeight)
     
     # Calculate x and y coordinates by linearly spacing bewteen max and min
     x_coords = np.linspace(min[0],max[0],tiledWidth+1)
@@ -14,10 +14,10 @@ def mosaic(function, pixelSize, min, max, tiledSize, folderPath):
     for row in range(tiledHeight):
         for column in range(tiledWidth):
             tile = anim.animation()
-            tile.addKeyframe(function=function,
+            tile.addKeyframe(anim.keyframe(function=function,
                              min=(x_coords[row], y_coords[column]),
                              max=(x_coords[row+1], y_coords[column+1]),
-                             length=1)
+                             length=1))
             tile.render(size=tilePixelSize, folderPath=folderPath)
             
     
--- myAnimation.py
+++ myAnimation.py
@@ -1,10 +1,15 @@
 import complexAnimation as anim
+import mosaic
 
+#myanim = anim.animation()
+#a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
+#myanim.addKeyframe(a)
 
-myanim = anim.animation()
-a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
-myanim.addKeyframe(a)
-
-
-myanim.render(size=(1440*5,1440*5), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
+#myanim.render(size=(1440*5,1440*5), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
 #myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
+
+mosaic.mosaic(function="(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",
+              pixelSize=(1920,1080),
+              min=(-4,-4), max=(4,4),
+              tiledSize=(4,4),
+              folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\mosaic")
