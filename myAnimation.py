776e39cfb631831ab35cabbbef9205b46a4db694
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-08-29 14:55:23 -0400

    Tried to git gitignore to work

--- myAnimation.py
+++ myAnimation.py
@@ -1,15 +1,14 @@
 import complexAnimation as anim
 import mosaic
 
-#myanim = anim.animation()
-#a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
-#myanim.addKeyframe(a)
+myanim = anim.animation()
+a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
+myanim.addKeyframe(a)
 
-#myanim.render(size=(1440*5,1440*5), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
-#myanim.render(size=(1440,1440), folderPath="C:\\Users\\trevo\\Desktop\\Grapher_Output")
+myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
 
-mosaic.mosaic(function="x",
-              pixelSize=(1080,1080),
-              min=(-4,-4), max=(4,4),
-              tiledSize=(2,2),
-              folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\mosaic")
+#mosaic.mosaic(function="x^(1j)",
+#              pixelSize=(1080,1080),
+#              min=(-4,-4), max=(4,4),
+#              tiledSize=(2,2),
+#              folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\mosaic")
