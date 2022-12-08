afb91432f1afc4292c1e1a8fbd405eb6105a0089
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2022-12-07 22:09:34 -0500

    Update myAnimation.py

--- myAnimation.py
+++ myAnimation.py
@@ -3,12 +3,14 @@ import mosaic
 
 myanim = anim.animation()
 a = anim.keyframe(function = "(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",  min=(-4,-4), max=(4,4), length=60)
+b = anim.keyframe(function = "x", min=(-4,-4), max=(4,4), length = 60)
 myanim.addKeyframe(a)
+myanim.addKeyframe(b)
 
-#myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fib(x)")
+myanim.render(size=(1440,1440), folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\f(x)=fancyfunc")
 
-mosaic.mosaic(function="(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",
-              pixelSize=(1920*32, 1080*32),
-              min=(-1,-1080/1920), max=(1,1080/1920),
-              tiledSize=(16,16),
-              folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\mosaic")
+#mosaic.mosaic(function="(x+1j*np.sin(x))/(1j*np.tanh(1/(x**2j)))",
+#              pixelSize=(1920*32, 1080*32),
+#              min=(-1,-1080/1920), max=(1,1080/1920),
+#              tiledSize=(16,16),
+#              folderPath="C:\\Users\\TAK\\Desktop\\Python\\Grapher_Output\\mosaic")
